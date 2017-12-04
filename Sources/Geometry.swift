6608dec553eb481816a9d6b1c84861c69ddfd074
Vladas Zakrevskis <146100@gmail.com>
2017-12-04 15:44:56 +0300

    Created CircleProgressView

--- Sources/Geometry.swift
+++ Sources/Geometry.swift
@@ -8,15 +8,35 @@
 
 import UIKit
 
-extension CGSize {
-    init(_ width: CGFloat, _ height: CGFloat) {
-        self.init(width: width, height: height)
+public extension CGRect {
+    
+    var center: CGPoint {
+        return CGPoint(origin.x + size.width / 2, origin.y + size.height / 2)
+    }
+    
+    init(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) {
+        self.init(x: x, y: y, width: width, height: height)
+    }
+    
+    init(center: CGPoint, width: CGFloat, height: CGFloat) {
+        self.init(center.x - width / 2, center.y - height / 2, width, height)
     }
 }
 
+public extension CGSize {
+    init(_ width: CGFloat, _ height: CGFloat) { self.init(width: width, height: height) }
+}
 
-extension CGPoint {
-    init(_ x: CGFloat, _ y: CGFloat) {
-        self.init(x: x, y: y)
+public extension CGPoint {
+    
+    init(_ x: CGFloat, _ y: CGFloat) { self.init(x: x, y: y) }
+    
+    static func onCircleWith(radius: CGFloat, angle: CGFloat, center: CGPoint) -> CGPoint {
+        return CGPoint(radius * cos(angle) + center.x, radius * sin(angle) + center.y)
+    }
+    
+    func midPointWith(_ point: CGPoint) -> CGPoint {
+        return CGPoint((x + point.x) / 2, (y + point.y) / 2)
     }
 }
+
