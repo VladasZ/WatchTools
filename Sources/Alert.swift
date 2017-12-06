046de305c972db3dd042ffca9d8af61c7debe68b
Vladas Zakrevskis <146100@gmail.com>
2017-12-06 11:39:49 +0300

    Hello

--- Sources/Alert.swift
+++ Sources/Alert.swift
@@ -11,6 +11,7 @@ import WatchKit
 public class Alert {
     
     public static func show(_ message: String) {
+        
         let action = WKAlertAction(title: "OK", style: .default, handler: { })
         rootController.presentAlert(withTitle: message, message: nil, preferredStyle: .alert, actions: [action])
     }
